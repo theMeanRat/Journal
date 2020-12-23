5832ec65014d2a4cccecfa097675443bd5d204df
mbaars@myvisions.nl <6csaB5L7x8Ekjc>
2020-12-23 16:31:06 +0100

    Categories can have multiple articles.

--- tests/Unit/ArticleCategoryTest.php
+++ tests/Unit/ArticleCategoryTest.php
@@ -5,6 +5,7 @@ namespace MyVisions\Journal\Tests\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use MyVisions\Journal\Tests\TestCase;
 use MyVisions\Journal\Models\ArticleCategory;
+use MyVisions\Journal\Models\Article;
 
 class ArticleCategoryTest extends TestCase
 {
@@ -18,5 +19,80 @@ class ArticleCategoryTest extends TestCase
         ]);
 
         $this->assertEquals('Fake Category Name', $articleCategory->name);
+    }
+
+    /** @test */
+    public function an_article_category_has_many_articles()
+    {
+        $firstArticleCategory = ArticleCategory::factory()->create([
+            'name' => 'First category'
+        ]);
+
+        $secondArticleCategory = ArticleCategory::factory()->create([
+            'name' => 'Second category'
+        ]);
+
+        Article::factory()->create([
+            'subtitle' => 'My first fake subtitle',
+            'introduction' => 'My first fake introduction',
+            'content' => 'Fake Content',
+            'main_image' => 'fake/image/path',
+            'active' => true,
+            'author_id' => 999,
+            'article_category_id' => $firstArticleCategory->id,
+            'slug' => 'my-first-fake-title',
+            'title' => 'Fake First Title'
+        ]);
+
+        Article::factory()->create([
+            'subtitle' => 'My second fake subtitle',
+            'introduction' => 'My second fake introduction',
+            'content' => 'Fake Content',
+            'main_image' => 'fake/image/path',
+            'active' => true,
+            'author_id' => 999,
+            'article_category_id' => $firstArticleCategory->id,
+            'slug' => 'my-second-fake-title',
+            'title' => 'Fake Second Title'
+        ]);
+
+        Article::factory()->create([
+            'subtitle' => 'My third fake subtitle',
+            'introduction' => 'My third fake introduction',
+            'content' => 'Fake Content',
+            'main_image' => 'fake/image/path',
+            'active' => true,
+            'author_id' => 999,
+            'article_category_id' => $firstArticleCategory->id,
+            'slug' => 'my-third-fake-title',
+            'title' => 'Fake Third Title'
+        ]);
+
+        Article::factory()->create([
+            'subtitle' => 'My fourth fake subtitle',
+            'introduction' => 'My fourth fake introduction',
+            'content' => 'Fake Content',
+            'main_image' => 'fake/image/path',
+            'active' => true,
+            'author_id' => 999,
+            'article_category_id' => $secondArticleCategory->id,
+            'slug' => 'my-fourth-fake-title',
+            'title' => 'Fake Fourth Title'
+        ]);
+
+        Article::factory()->create([
+            'subtitle' => 'My fifth fake subtitle',
+            'introduction' => 'My fifth fake introduction',
+            'content' => 'Fake Content',
+            'main_image' => 'fake/image/path',
+            'active' => true,
+            'author_id' => 999,
+            'article_category_id' => $secondArticleCategory->id,
+            'slug' => 'my-fifth-fake-title',
+            'title' => 'Fake Fifth Title'
+        ]);
+
+        $this->assertCount(3, $firstArticleCategory->articles()->get());
+        $this->assertCount(2, $secondArticleCategory->articles()->get());
     }
 }
