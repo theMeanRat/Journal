746cb222967e0d77b07e2fe48497252dfe614756
mbaars@myvisions.nl <6csaB5L7x8Ekjc>
2021-01-06 16:58:52 +0100

    Form input names in labels and article_id inline if to check existance.

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -4,34 +4,41 @@
 
         {{ csrf_field() }}
 
-        <input type="hidden" name="article_id" value="{{ $article->id }}" />
+        <input type="hidden" name="article_id" value="{{  isset($article) ? $article->id : 0 }}" />
 
         <div class="form_input_container col-start-2 col-span-3">
-            <input type="text" class="text" name="title" value="" />
+            <label for="title">Titel:</label>
+            <input id="title" type="text" class="text" name="title" value="" />
         </div>
 
         <div class="form_input_container col-start-2 col-span-3">
-            <input type="text" class="text" name="subtitle" value="" />
+            <label for="subtitle">Subitel:</label>
+            <input id="subtitle" type="text" class="text" name="subtitle" value="" />
         </div>
 
         <div class="form_input_container col-start-2 col-span-3">
-            <input type="text" class="text" name="introduction" value="" />
+            <label for="introduction">Inleiding:</label>
+            <input id="introduction" type="text" class="text" name="introduction" value="" />
         </div>
 
         <div class="form_input_container col-start-2 col-span-3">
-            <input type="text" class="text" name="content" value="" />
+            <label for="content">Inhoud:</label>
+            <input id="content" type="text" class="text" name="content" value="" />
         </div>
 
         <div class="form_input_container col-start-2 col-span-3">
-            <input type="text" class="text" name="main_image" value="" />
+            <label for="main_image">Afbeelding:</label>
+            <input id="main_image" type="text" class="text" name="main_image" value="" />
         </div>
 
         <div class="form_input_container col-start-1 col-span-1">
-            <input type="text" class="text" name="article_category_id" value="" />
+            <label for="article_category_id">Artikel Categorie:</label>
+            <input id="article_category_id" type="text" class="text" name="article_category_id" value="" />
         </div>
 
         <div class="form_input_container col-start-2 col-span-2">
-            <input type="text" class="text" name="slug" value="" />
+            <label for="slug">Slug:</label>
+            <input id="slug" type="text" class="text" name="slug" value="" />
         </div>
 
     </form>
